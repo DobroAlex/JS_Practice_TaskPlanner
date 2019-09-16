da3b399ef132b2fe04d81b0609ba17cb5fdaf68b
Alex <alexkoshernosiegov@gmail.com>
2019-09-16 11:46:44 +0300

    Fixed " DeprecationWarning: current Server Discovery and Monitoring engine" at mongo connection

--- server/database/mongo-connection.js
+++ server/database/mongo-connection.js
@@ -4,7 +4,7 @@ const SERVER_ADDR = 'mongodb://localhost:27017/UserTasks'
 
 async function connectToMongo (address = SERVER_ADDR) {
   try {
-    return await mongoose.connect(address, { useNewUrlParser: true })
+    return await mongoose.connect(address, { useNewUrlParser: true, useUnifiedTopology: true })
   } catch (e) {
     console.log(`Couldn't connect to mongo db at ${address}, you should abort execution \n ${e}`)
     return -1
