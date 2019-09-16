ef6c5a3ed6fcb0b27fe819d626e599d06918ebe2
Alex <alexkoshernosiegov@gmail.com>
2019-09-16 12:20:58 +0300

    Added password comparison function

--- server/src/libs/crypto.js
+++ server/src/libs/crypto.js
@@ -3,8 +3,13 @@ const sha = require('js-sha256')
 
 async function encryptPassword (rawPassword) {
   const salt = sha.sha256(Date.now())
-  const encrtyptedPass = await bcrypt.hash(rawPassword, salt)
-  return encrtyptedPass
+  const encrtyptedPassword = await bcrypt.hash(rawPassword, salt)
+  return encrtyptedPassword
 }
 
-module.exports = { encryptPassword }
+async function comparePassword (rawPassword, encrtyptedPassword) {
+  const result = await bcrypt.compare(rawPassword, encrtyptedPassword)
+  return result
+}
+
+module.exports = { encryptPassword, comparePassword }
