fb8c918726ddf6cf5fbf1912b5f3b4935f069985
Alex <alexkoshernosiegov@gmail.com>
2019-09-16 12:45:19 +0300

    Replaced sha-salt with rounds

--- server/src/libs/crypto.js
+++ server/src/libs/crypto.js
@@ -1,9 +1,7 @@
 const bcrypt = require('bcrypt')
-const sha = require('js-sha256')
 
 async function encryptPassword (rawPassword) {
-  const salt = sha.sha256(Date.now())
-  const encrtyptedPassword = await bcrypt.hash(rawPassword, salt)
+  const encrtyptedPassword = await bcrypt.hash(rawPassword, Number(process.env.SALT_ROUNDS))
   return encrtyptedPassword
 }
 
