7bd7c5bbc8513945a5a7b0518b68d7c7d2b9099d
Alex <alexkoshernosiegov@gmail.com>
2019-09-17 22:52:25 +0300

    Removed immutable parametr

--- server/src/libs/jwt-utils.js
+++ server/src/libs/jwt-utils.js
@@ -4,8 +4,8 @@ function newAccessToken ({ email, role }, expiration = process.env.JWT_DEFAULT_E
   return jwt.sign({ email, role }, process.env.JWT_SECRET, { expiresIn: expiration })
 }
 
-function verifyAccessToken (token, expiration = process.env.JWT_DEFAULT_EXPIRATION) {
-  return jwt.verify(token, process.env.JWT_SECRET, { expiresIn: expiration })
+function verifyAccessToken (token) {
+  return jwt.verify(token, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DEFAULT_EXPIRATION })
 }
 
 module.exports = { newAccessToken, verifyAccessToken }
