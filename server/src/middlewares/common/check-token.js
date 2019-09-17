bce0175de8df459368a634eb3e7e4fcfdfb884f1
Alex <alexkoshernosiegov@gmail.com>
2019-09-17 22:52:44 +0300

    Added middleware-like method to validate token

---
+++ server/src/middlewares/common/check-token.js
@@ -0,0 +1,22 @@
+const jwt = require('../../libs/jwt-utils')
+const utils = require('../../libs/utils')
+
+function getTokenFromHeader (ctx) {
+  if (!ctx.headers.authorization) {
+    throw utils.errorGenerator(400, 'No auth header in form of \'Bearer %token\' was found')
+  }
+  return ctx.headers.authorization.split(' ')[1].trim
+}
+
+function checkGivenToken (token) {
+  return jwt.verifyAccessToken(token)
+}
+
+async function checkToken (ctx, next) {
+  try {
+    return checkGivenToken(getTokenFromHeader(ctx))
+  } catch (e) {
+    throw utils.errorGenerator(422, `${e}`)
+  }
+}
+module.exports = { checkToken }
