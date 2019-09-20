4291e4b4093c5dec4501e9960b62cfc06aef297f
Alex <alexkoshernosiegov@gmail.com>
2019-09-20 21:43:41 +0300

    Added uniqueness and requirement to some fields

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  email: String,
-  password: String, // should be stored as bcrypted string
-  role: String,
+  email: { type: String, unique: true, required: true, dropDups: true },
+  password: { type: String, required: true }, // should be stored as bcrypted string
+  role: { type: String, required: true },
   tasks: [{
     date: Date,
     remindBefore: Date, // notification should be sent at date - remindBefore
