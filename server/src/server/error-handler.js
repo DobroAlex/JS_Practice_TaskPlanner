264f5f0b19728503010e88e6bd35d77d96bfec78
Alex <alexkoshernosiegov@gmail.com>
2019-09-22 17:59:04 +0300

    Removed unused log

--- server/src/server/error-handler.js
+++ server/src/server/error-handler.js
@@ -2,7 +2,7 @@ async function errorHandle (context, next) {
   try {
     await next()
   } catch (e) {
-    console.log(`Error occured: ${e} \n ${e.stack}`)
+    console.log(`Error occured: ${e}`)
     context.status = 500 // Iternal server error as default, should be normally changed by middlewares
     if (e.status) {
       context.status = e.status
